7d90b6389b3d943de506ac0f57d1a23e534e8fe8
ElcimarSilva <elcimar8@gmail.com>
2022-03-08 17:59:40 -0300

    melhorias

--- logg.py
+++ logg.py
@@ -1,17 +1,11 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s') # para passar data e hora + level do log + mensagem do log
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s') # para passar data e hora + level do log + mensagem do log
 # logging.basicConfig(level=logging.DEBUG, filename='log.log') # Para mandar para um arquivo
 
-""" logging.debug("Debug!!")
-
-logging.info("Info!!")
-
-logging.warning("Warning!!")
-
-logging.error("Error!!") """
-
-
-
 variavel = 'cloudwatch'
+logging.debug({"é assim que se faz logs para mandar para cloudwatch":variavel}) # este não aparece a menos que coloque lvl debug
 logging.info({"é assim que se faz logs para mandar para cloudwatch":variavel})
+logging.warning({"é assim que se faz logs para mandar para cloudwatch":variavel})
+logging.error({"é assim que se faz logs para mandar para cloudwatch":variavel})
+logging.critical({"é assim que se faz logs para mandar para cloudwatch":variavel})
